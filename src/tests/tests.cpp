59754d7612afac72a8c3e8531dbdb32bc6417df2
Mateusz Pusz <mateusz.pusz@gmail.com>
2017-04-10 20:09:08 +0200

    Added unit tests for move assignment operator

--- src/tests/tests.cpp
+++ src/tests/tests.cpp
@@ -263,3 +263,53 @@ TYPED_TEST(optTest, copyAssignmentNotEmptyForNotEmpty)
   EXPECT_EQ(this->value, o2.value());
   EXPECT_EQ(this->value, o2.value_or(this->other_value));
 }
+
+TYPED_TEST(optTest, moveAssignmentEmptyForEmpty)
+{
+  using opt_type = typename TestFixture::type;
+  opt_type o1;
+  opt_type o2;
+  o2 = std::move(o1);
+  EXPECT_FALSE(o2);
+  EXPECT_FALSE(o2.has_value());
+  EXPECT_THROW(o2.value(), bad_optional_access);
+  EXPECT_EQ(this->other_value, o2.value_or(this->other_value));
+}
+
+TYPED_TEST(optTest, moveAssignmentNotEmptyForEmpty)
+{
+  using opt_type = typename TestFixture::type;
+  opt_type o1{this->value};
+  opt_type o2;
+  o2 = std::move(o1);
+  EXPECT_TRUE(o2);
+  EXPECT_TRUE(o2.has_value());
+  EXPECT_EQ(this->value, *o2);
+  EXPECT_EQ(this->value, o2.value());
+  EXPECT_EQ(this->value, o2.value_or(this->other_value));
+}
+
+TYPED_TEST(optTest, moveAssignmentEmptyForNotEmpty)
+{
+  using opt_type = typename TestFixture::type;
+  opt_type o1;
+  opt_type o2{this->value};
+  o2 = std::move(o1);
+  EXPECT_FALSE(o2);
+  EXPECT_FALSE(o2.has_value());
+  EXPECT_THROW(o2.value(), bad_optional_access);
+  EXPECT_EQ(this->other_value, o2.value_or(this->other_value));
+}
+
+TYPED_TEST(optTest, moveAssignmentNotEmptyForNotEmpty)
+{
+  using opt_type = typename TestFixture::type;
+  opt_type o1{this->value};
+  opt_type o2{this->other_value};
+  o2 = std::move(o1);
+  EXPECT_TRUE(o2);
+  EXPECT_TRUE(o2.has_value());
+  EXPECT_EQ(this->value, *o2);
+  EXPECT_EQ(this->value, o2.value());
+  EXPECT_EQ(this->value, o2.value_or(this->other_value));
+}
